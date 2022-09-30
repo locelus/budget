d2cbb5e37ba4594fb714f80387af1bec09ff8240
locelus <spfletcher2003@gmail.com>
2022-09-29 17:07:37 -0700

    Tutorial

---
+++ pages/bio.js
@@ -0,0 +1,10 @@
+const Bio = () => {
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">Name</p>
+      <p className="text-center">Bio here</p>
+    </div>
+  )
+}
+
+export default Bio
